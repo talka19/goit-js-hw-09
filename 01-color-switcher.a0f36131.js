5064de88f481d44650fe1d257304d749198a95ee
talka19 <111132978+talka19@users.noreply.github.com>
2023-01-22 12:38:59 +0000

    Deploying to gh-pages from @ talka19/goit-js-hw-09@5879ab4c57cc5f50236bdc85248219b994fa6672 🚀

---
+++ 01-color-switcher.a0f36131.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(function(){t.disabled=!0,n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.a0f36131.js.map
---
+++ 01-color-switcher.c1f50b3b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=null;t.addEventListener("click",(function(){t.disabled=!0,e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){t.disabled=!1,clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.c1f50b3b.js.map
